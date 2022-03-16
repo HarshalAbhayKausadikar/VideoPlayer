51aa911d69d4d73807cd6196939b636ee487c00f
HarshalAbhayKausadikar <harshalkausadikar@gmail.com>
2022-03-16 19:05:53 +0530

    added navbar successfully

--- index.php
+++ index.php
@@ -6,21 +6,42 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
     <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
-        crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-        crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="resource\css\bootstrap.min.css">
+    <script src="resource/js/slim.min.js"></script>
+    <script src="resource/js/popper.min.js"></script>
+    <script src="resource/js/bootstrap.min.js"></script>
 </head>
 
 <body>
-    <h3 class="heading"> Video Gallery</h3>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-dark" id="bg-color">
+    <a class="navbar-brand" href="#">
+      <img src="resource\black_logo.png" width="30" height="30" class="d-inline-block align-top" alt="" id="bg-color">
+      EasyCode
+    </a>
+  
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Hi, Learner!
+          </a>
+          <div class="dropdown-menu" id="navbarDropdown" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" id="navbarDropdown" href="#">Home</a>
+            <a class="dropdown-item" id="navbarDropdown" href="#">Courses</a>  
+            <div class="dropdown-divider" id="navbarDropdown"></div>
+            <a class="dropdown-item" id="navbarDropdown" href="#">Logout</a>
+          </div>
+        </li>
+      </ul>
+    </div>
+  </nav>
+    <h3 class="heading">Python Programming A to Z</h3>
 
     <div class="container">
 
